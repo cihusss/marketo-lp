3bcd7771d66ca94fec413a3f453e7ceb519aaeac
Teo Milicevic <teo-milicevic@bisk.com>
2020-12-29 10:01:45 -0500

    added js file

---
+++ js/main.js
@@ -0,0 +1,15 @@
+kWidget.embed({
+    "targetId": "k_player01",
+    "wid": "_2353281",
+    "uiconf_id": "45992131",
+    "flashvars": {},
+    "entry_id": "1_bi8dat82"
+    });
+
+kWidget.embed({
+    "targetId": "k_player02",
+    "wid": "_2353281",
+    "uiconf_id": "45992131",
+    "flashvars": {},
+    "entry_id": "1_bi8dat82"
+});
