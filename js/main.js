07f17069e0287346c59a7a9a0d93031128ec7242
Teo Milicevic <teo-milicevic@bisk.com>
2021-01-07 12:22:10 -0500

    kaltura in main.js

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
-// kWidget.embed({
-//     "targetId": "k-player-small",
-//     "wid": "_2353281",
-//     "uiconf_id": "45992131",
-//     "flashvars": {},
-//     "entry_id": "1_bi8dat82"
-//     });
+kWidget.embed({
+    "targetId": "k-player-small",
+    "wid": "_2353281",
+    "uiconf_id": "45992131",
+    "flashvars": {},
+    "entry_id": "1_bi8dat82"
+    });
 
-// kWidget.embed({
-//     "targetId": "k-player-large",
-//     "wid": "_2353281",
-//     "uiconf_id": "45992131",
-//     "flashvars": {},
-//     "entry_id": "1_bi8dat82"
-// });
+kWidget.embed({
+    "targetId": "k-player-large",
+    "wid": "_2353281",
+    "uiconf_id": "45992131",
+    "flashvars": {},
+    "entry_id": "1_083v7xfq"
+});
